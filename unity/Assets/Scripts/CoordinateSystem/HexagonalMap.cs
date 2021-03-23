7b3e91637bc0cce919550f1d66847df57a9dabe6
Monteiro Simoes Ricardo <monteric@students.zhaw.ch>
2021-03-23 22:47:31 +0100

    Changes map from generic to fixed for Tiles

--- unity/Assets/Scripts/CoordinateSystem/HexagonalMap.cs
+++ unity/Assets/Scripts/CoordinateSystem/HexagonalMap.cs
@@ -1,22 +1,24 @@
+using Hexxle.ClassSystem;
+
 namespace Hexxle.CoordinateSystem
 {
-    public class Hexagonal<T> where T : class, new()
+    public class HexagonalMap
     {
-        private Axes<Axes<Axes<T>>> _axes = new Axes<Axes<Axes<T>>>();
+        private Axes<Axes<Axes<Tile>>> _axes = new Axes<Axes<Axes<Tile>>>();
 
 
-        public T this[int X, int Y, int Z]
+        public Tile this[int X, int Y, int Z]
         {
             get => _axes[X][Y][Z];
             set => _axes[X][Y][Z] = value;
         }
 
-
-        public T this[Coordinate coordinate]
+        public Tile this[Coordinate coordinate]
         {
             get => this[coordinate.x, coordinate.y, coordinate.z];
             set => this[coordinate.x, coordinate.y, coordinate.z] = value;
         }
+
 
     }
 }

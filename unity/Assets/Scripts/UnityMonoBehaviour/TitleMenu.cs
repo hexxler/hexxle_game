0eca5d31ebd54bf855e1e6e0f055e6b7f12fd147
Akcaden1 <akcaden1@students.zhaw.ch>
2021-03-27 21:34:55 +0100

    add titlescreen

---
+++ unity/Assets/Scripts/UnityMonoBehaviour/TitleMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("play_game");
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit_game");
+        Application.Quit();
+    }
+}

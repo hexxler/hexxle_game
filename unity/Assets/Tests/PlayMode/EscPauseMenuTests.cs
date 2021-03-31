9e574b091007be08b41014a8da4426c6df5b30b9
Ricardo Simoes <ricardo_simoes_93@hotmail.com>
2021-03-31 21:58:57 +0200

    Adds PlayMode test for PauseMenu invocation

---
+++ unity/Assets/Tests/PlayMode/EscPauseMenuTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Tests
+{
+    public class EscPauseMenuTests : InputTestFixture
+    {
+        Keyboard keyboard;
+        Mouse mouse;
+        InputManager inputManager;
+        InputAction escapePressAction;
+
+        [SetUp]
+        public void Setup()
+        {
+            keyboard = InputSystem.AddDevice<Keyboard>();
+            mouse = InputSystem.AddDevice<Mouse>();
+            inputManager = new InputManager();
+            escapePressAction = inputManager.MenuInteraction.Pause;
+            SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        }
+        [UnityTest]
+        public IEnumerator PressingEscChangesFromMainSceneToTitlescreen()
+        {
+            Assert.AreEqual("Main", SceneManager.GetActiveScene().name);
+            escapePressAction.Enable();
+            Press(keyboard.escapeKey);
+            using (var trace = new InputActionTrace())
+            {
+                trace.SubscribeTo(escapePressAction);
+
+
+                escapePressAction.Disable();
+
+                //Checks to see if the action was executed exactly once
+                Assert.AreEqual(1, trace.count);
+            }
+
+            yield return new WaitForSecondsRealtime(1);
+
+
+            Assert.AreEqual("titlescreen", SceneManager.GetActiveScene().name);
+
+        }
+
+
+    }
+}

460c9254e76b32b6f18d98f19472e165ec128eb3
Akcaden1 <akcaden1@students.zhaw.ch>
2021-04-03 22:00:08 +0200

    adds basic sound components

---
+++ unity/Assets/Scripts/Unity/Audio/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        //start main theme here
+        //Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
---
+++ unity/Assets/Scripts/Unity/Audio/Sound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound{
+
+    public string name;
+    public AudioClip clip;
+    [Range(0,1)]
+    public float volume;
+    [Range(0,1)]
+    public float pitch;
+    public AudioSource source;
+    public bool loop = false;
+
+}

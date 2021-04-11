4fccb10785bb85cda1a2ef0aced0b5a7fb631cf9
Dennis Bannerman <dennis@bannerman.ch>
2021-04-11 19:54:59 +0200

    Added functionality to pause menu

---
+++ unity/Assets/Scripts/Unity/UnityPause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UnityPause : MonoBehaviour
+{
+    public void ResumeRound()
+    {
+        var uiPanel = GameObject.FindGameObjectWithTag("UI").transform.GetChild(0).gameObject;
+
+        // deactivate PausePanel
+        var pauseCanvas = GameObject.FindGameObjectWithTag("Pause");
+        pauseCanvas.transform.GetChild(0).gameObject.SetActive(false);
+
+        // enable Buttons in UI
+        foreach (Button button in uiPanel.GetComponentsInChildren<Button>())
+        {
+            button.enabled = true;
+        }
+    }
+
+    public void ShowInfo()
+    {
+
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("titlescreen", LoadSceneMode.Single);
+    }
+}
--- unity/Assets/Scripts/Unity/UnityUI.cs
+++ unity/Assets/Scripts/Unity/UnityUI.cs
@@ -6,14 +6,11 @@ namespace Hexxle.Unity
 {
     public class UnityUI : MonoBehaviour
     {
-        public void exitRound()
-        {
-            SceneManager.LoadScene("titlescreen", LoadSceneMode.Single);
-        }
-
         public void PauseRound()
         {
             var uiPanel = GameObject.FindGameObjectWithTag("UI").transform.GetChild(0).gameObject;
+
+            // activate PausePanel
             var pauseCanvas = GameObject.FindGameObjectWithTag("Pause");
             pauseCanvas.transform.GetChild(0).gameObject.SetActive(true);
 

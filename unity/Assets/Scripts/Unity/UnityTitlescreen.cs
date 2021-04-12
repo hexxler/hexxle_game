cb09db9231c58c3e0da34e1dbc4a2cfa7c55462e
adri <adri_mar_q@hotmail.com>
2021-04-12 15:52:06 +0200

    Added Info Panel to TitleScreen and a new Background

--- unity/Assets/Scripts/Unity/UnityTitlescreen.cs
+++ unity/Assets/Scripts/Unity/UnityTitlescreen.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Util;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Hexxle.Unity
 {
     public class UnityTitlescreen : MonoBehaviour
     {
+
+        void Start()
+        {
+            transform.GetChild(2).gameObject.SetActive(false); // InfoPanel
+            transform.GetChild(3).gameObject.SetActive(false); // Return Button
+        }
+
         public void PlayGame()
         {
-            Debug.Log("Play_game");
             //play soundeffect
             FindObjectOfType<AudioManager>().Play(GameSoundTypes.POP);
 
@@ -16,11 +23,26 @@ namespace Hexxle.Unity
 
         public void QuitGame()
         {
-            Debug.Log("Quit_game");
             //play soundeffect
             FindObjectOfType<AudioManager>().Play(GameSoundTypes.POP);
 
             Application.Quit();
+        }
+
+        public void GetInfoPanel()
+        {
+            transform.GetChild(2).gameObject.SetActive(true);   // Info Panel
+            transform.GetChild(3).gameObject.SetActive(true);   // Return Button 
+            transform.GetChild(1).gameObject.SetActive(false);  // TitleScreen
+            FindObjectOfType<AudioManager>().Play(GameSoundTypes.POP);
+        }
+
+        public void ReturnToTitle()
+        {
+            transform.GetChild(2).gameObject.SetActive(false);   // Info Panel
+            transform.GetChild(3).gameObject.SetActive(false);   // Return Button 
+            transform.GetChild(1).gameObject.SetActive(true);  // TitleScreen
+            FindObjectOfType<AudioManager>().Play(GameSoundTypes.POP);
         }
     }
 }

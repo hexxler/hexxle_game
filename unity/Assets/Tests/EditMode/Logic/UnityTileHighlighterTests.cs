5f7d6e8ee7cc261750b3c0f2e36ad11329cc28b2
Ricardo Simoes <ricardo_simoes_93@hotmail.com>
2021-04-13 23:53:34 +0200

    Adds tests for tile highlighter

---
+++ unity/Assets/Tests/EditMode/Logic/UnityTileHighlighterTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Hexxle.Unity;
+
+namespace Hexxle.Tests.Unity
+{
+    public class UnityTileHighlighterTests
+    {
+        UnityTileHighlighter tileHighlighter;
+
+        [SetUp]
+        public void Setup()
+        {
+            tileHighlighter = new UnityTileHighlighter();
+        }
+
+
+        [Test]
+        public void TurningHighlighterOnSetsInteralVariableToTrue()
+        {
+            Assert.IsFalse(tileHighlighter.isHighlighted);
+            tileHighlighter.TurnOn();
+            Assert.IsTrue(tileHighlighter.isHighlighted);
+
+        }
+
+        [Test]
+        public void TurningHighlighterOffSetsInteralVariableToFalse()
+        {
+            Assert.IsFalse(tileHighlighter.isHighlighted);
+            tileHighlighter.TurnOn();
+            Assert.IsTrue(tileHighlighter.isHighlighted);
+            tileHighlighter.TurnOff();
+            Assert.IsFalse(tileHighlighter.isHighlighted);
+
+        }
+    }
+}

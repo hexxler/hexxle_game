612bc8375ec062fd4ca40433c7a17cfb59cd2ddb
Dennis Bannerman <dennis@bannerman.ch>
2021-04-14 00:42:03 +0200

    Added tests for natures, types, tiles

---
+++ unity/Assets/Tests/EditMode/TileSystem/Nature/NatureTests.cs
@@ -0,0 +1,77 @@
+﻿using Hexxle.CoordinateSystem;
+using Hexxle.Interfaces;
+using Hexxle.TileSystem;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexxle.Tests.TileSystem.Nature
+{
+    public class NatureTests
+    {
+        ITile tile;
+        List<Coordinate> expectedCoordinates;
+        List<Coordinate> actualCoordinates;
+
+        [SetUp]
+        public void Setup()
+        {
+            tile = null;
+            expectedCoordinates = new List<Coordinate>();
+            actualCoordinates = new List<Coordinate>();
+        }
+
+        [Test]
+        public void AdjacentCoordinates()
+        {
+            int origin_x = 5;
+            int origin_y = -5;
+            int origin_z = 0;
+            Coordinate origin = new Coordinate(origin_x, origin_y, origin_z);
+
+            tile = Tile.CreateInstance(EState.None, EType.None, ENature.Circle, EBehaviour.None);
+            tile.Coordinate = origin;
+
+            expectedCoordinates.AddRange(new List<Coordinate>
+            {
+                new Coordinate(origin_x + 1, origin_y - 1, origin_z),
+                new Coordinate(origin_x + 1, origin_y, origin_z - 1),
+                new Coordinate(origin_x - 1, origin_y + 1, origin_z),
+                new Coordinate(origin_x - 1, origin_y, origin_z + 1),
+                new Coordinate(origin_x, origin_y - 1, origin_z + 1),
+                new Coordinate(origin_x, origin_y + 1, origin_z - 1)
+            });
+
+            actualCoordinates = tile.Nature.RelevantCoordinates(tile.Coordinate);
+
+            expectedCoordinates.ForEach(coord =>
+            {
+                Assert.True(actualCoordinates.Contains(coord));
+            });
+        }
+
+        [Test]
+        public void RelevantCoordinates_Circle()
+        {
+            int origin_x = 23;
+            int origin_y = 0;
+            int origin_z = -23;
+            Coordinate origin = new Coordinate(origin_x, origin_y, origin_z);
+
+            tile = Tile.CreateInstance(EState.None, EType.None, ENature.Circle, EBehaviour.None);
+            tile.Coordinate = origin;
+
+            expectedCoordinates.AddRange(tile.Nature.AdjacentCoordinates(tile.Coordinate));
+
+            actualCoordinates = tile.Nature.RelevantCoordinates(origin);
+
+            expectedCoordinates.ForEach(coord =>
+            {
+                Assert.True(actualCoordinates.Contains(coord));
+            });
+        }
+    }
+}
---
+++ unity/Assets/Tests/EditMode/TileSystem/TileTests.cs
@@ -0,0 +1,57 @@
+﻿using Hexxle.Interfaces;
+using Hexxle.TileSystem;
+using Hexxle.TileSystem.Behaviour;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexxle.Tests.TileSystem
+{
+    public class TileTests
+    {
+        ITile tile;
+
+        [Test]
+        public void CreateInstance_Test()
+        {
+            Random r = new Random();
+            EState expectedState = (EState)r.Next(1, Enum.GetValues(typeof(EState)).Length);
+            EType expectedType = (EType)r.Next(1, Enum.GetValues(typeof(EType)).Length);
+            ENature expectedNature = (ENature)r.Next(1, Enum.GetValues(typeof(ENature)).Length);
+            EBehaviour expectedBehaviour = (EBehaviour)r.Next(1, Enum.GetValues(typeof(EBehaviour)).Length);
+
+            tile = Tile.CreateInstance(expectedState, expectedType, expectedNature, expectedBehaviour);
+            Assert.AreEqual(expectedState, tile.State);
+            Assert.AreEqual(expectedType, tile.Type.Type);
+            Assert.AreEqual(expectedNature, tile.Nature.Nature);
+            Assert.AreEqual(expectedBehaviour, tile.Behaviour.Behaviour);
+        }
+
+        public void CreateInstance_NoState()
+        {
+            tile = Tile.CreateInstance(EState.None, EType.None, ENature.None, EBehaviour.None);
+            Assert.Equals(tile.State, EState.None);
+        }
+
+        public void CreateInstance_NoType()
+        {
+            tile = Tile.CreateInstance(EState.None, EType.None, ENature.None, EBehaviour.None);
+            Assert.IsNull(tile.Type);
+        }
+
+        public void CreateInstance_NoNature()
+        {
+            tile = Tile.CreateInstance(EState.None, EType.None, ENature.None, EBehaviour.None);
+            Assert.IsNull(tile.Nature);
+        }
+
+        public void CreateInstance_NoBehaviour()
+        {
+            tile = Tile.CreateInstance(EState.None, EType.None, ENature.None, EBehaviour.None);
+            Assert.IsInstanceOf(typeof(NoEffectBehaviour), tile.Behaviour);
+        }
+    }
+}

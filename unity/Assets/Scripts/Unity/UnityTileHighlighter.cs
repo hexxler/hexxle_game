356b44d960cfd4f4bd04fbf6b458bb4bc0315787
Ricardo Simoes <ricardo_simoes_93@hotmail.com>
2021-04-14 23:40:19 +0200

    Fixes bug where tile highlight was being set to on and off simultanously

--- unity/Assets/Scripts/Unity/UnityTileHighlighter.cs
+++ unity/Assets/Scripts/Unity/UnityTileHighlighter.cs
@@ -6,6 +6,7 @@ namespace Hexxle.Unity
     {
 
         public bool isHighlighted { get; private set; }
+        private bool setToBlackOnce;
         //smaller -> slower
         private readonly float INVERSE_PERIOD_DURATION = 0.4f;
         private readonly float MAX_EMISSION_POWER = 0.2f;
@@ -30,14 +31,13 @@ namespace Hexxle.Unity
                 var material = GetComponent<Renderer>().material;
                 var emission = Mathf.PingPong(Time.time * INVERSE_PERIOD_DURATION, MAX_EMISSION_POWER);
                 emission = Mathf.LinearToGammaSpace(emission);
-                material.EnableKeyword("_Emission");
                 material.SetColor("_EmissionColor", GLOW_COLOR * emission);
             }
-            else
+            else if(setToBlackOnce)
             {
                 var material = GetComponent<Renderer>().material;
                 material.SetColor("_EmissionColor", Color.black);
-                material.DisableKeyword("_Emission");
+                setToBlackOnce = false;
             }
         }
 
@@ -45,12 +45,14 @@ namespace Hexxle.Unity
         {
             Debug.Log("Turning highlight on");
             isHighlighted = true;
+            setToBlackOnce = false;
         }
 
         public void TurnOff()
         {
             Debug.Log("Turning highlight off");
             isHighlighted = false;
+            setToBlackOnce = true;
         }
 
         

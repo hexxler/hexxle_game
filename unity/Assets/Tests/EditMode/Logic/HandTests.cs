e1257e96e5624357e3d8f1fcda9cfe0cce4a1765
Akcaden1 <akcaden1@students.zhaw.ch>
2021-04-20 09:24:01 +0200

    add unit test for hand class

---
+++ unity/Assets/Tests/EditMode/Logic/HandTests.cs
@@ -0,0 +1,39 @@
+﻿using Hexxle.Logic;
+using Hexxle.Interfaces;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace Hexxle.Tests.Logic
+{
+    public class HandTests
+    {
+        Hand hand;
+        TileStack stack;
+        int HandSize = 5;
+        List<ITile> FirstTiles;
+
+        [SetUp]
+        public void Setup()
+        {
+            stack = new TileStack();
+
+            FirstTiles = new List<ITile>();
+            for(int i = 0; i < HandSize; i++)
+            {
+                FirstTiles.Add(stack.Pop());
+            }
+
+            hand = new Hand(HandSize, FirstTiles);
+        }
+
+        [Test]
+        public void CheckFirstTiles()
+        {
+            CollectionAssert.AreEqual(FirstTiles, hand.GetTiles());
+        }
+
+    }
+}

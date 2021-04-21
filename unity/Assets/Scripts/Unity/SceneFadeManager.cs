ba6d60e620ca560c704304eb93c5926d328ebb57
N2Drew <andrew.surber@ymail.com>
2021-04-21 21:34:17 +0200

    Adds overhauled titlescreen, placeholder options dialog, crossfade between scenes and progress bar for points

---
+++ unity/Assets/Scripts/Unity/SceneFadeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFadeManager : MonoBehaviour
+{
+    public static SceneFadeManager instance;
+    public Animator transition;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void LoadTransitionScene(string sceneName, LoadSceneMode mode)
+    {
+        StartCoroutine(TransitionScene(sceneName, mode));
+    }
+
+    IEnumerator TransitionScene(string sceneName, LoadSceneMode mode)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName, mode);
+    }
+}
